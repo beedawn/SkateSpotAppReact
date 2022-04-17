0e628c3dae8e64ce19803d117000935b7b433032
bee-dmg <brandonschmersal@gmail.com>
2022-04-17 09:39:10 -0400

    Adding more comments

--- src/routes/Spots.js
+++ src/routes/Spots.js
@@ -1,7 +1,9 @@
 export default function Spots() {
-    return (
+  //need to add query to db to pull all spots?  
+  return (
       <div style={{ padding: "1rem 0" }}>
         <h2>Spots</h2>
+        
       </div>
     );
   }
