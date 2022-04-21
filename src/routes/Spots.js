c81b118b37c16b4a33aa495ae6315affe52d9999
bee-dmg <brandonschmersal@gmail.com>
2022-04-21 14:50:16 -0400

    Adding Edit button to Spots Component.

--- src/routes/Spots.js
+++ src/routes/Spots.js
@@ -1,10 +1,11 @@
 import { onSnapshot, collection, doc, getDocs } from "firebase/firestore";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import { db } from "../firebase-config";
 import loading from "../images/Loading_icon.gif";
-
+import AuthContext from "../context/AuthContext";
+import { Button } from "reactstrap";
 export default function Spots() {
-  
+  const { user } = useContext(AuthContext);
   const [spots, setSpots] = useState([]);
   useEffect(() => {
     const unsub = onSnapshot(collection(db, "spots"), (snapshot) => {
@@ -19,10 +20,12 @@ export default function Spots() {
        <div style={{padding:"1rem"}}> <h2>Spots</h2></div>
         {spots.map((spot) => (
           <div style={{padding:"1rem 0"}}>
+            {console.log(spot.admin)}
             <div key={spot.id}>
+            
               <h4>{spot.name}</h4>
               <h5>{spot.location}</h5>
-              <div>{/* <p>Image of the spot</p> */}</div>
+              <div>{user.email===spot.admin ? <Button color="primary" onClick={() => {}}> Edit </Button> : <p></p>}</div>
               <p>{spot.description}</p>
             </div>
           </div>
