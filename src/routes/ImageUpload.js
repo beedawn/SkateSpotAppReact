f566e4c4e11f909c26283234680bd3d0a150205c
bee-dmg <brandonschmersal@gmail.com>
2022-04-24 13:20:06 -0400

    Working more with image upload.

--- src/routes/ImageUpload.js
+++ src/routes/ImageUpload.js
@@ -13,37 +13,47 @@ export default function ImageUpload() {
   const { user } = useContext(AuthContext);
 
   const [imageUpload, setImageUpload] = useState(null);
-
+const [check, setCheck] =useState("true");
  
  const handleUpload = () => {
+    //  if(imageUpload.size > 500000){
+    //      setCheck("false");
+    //      return(
+    //          console.log("File is too Large!")
+    //      )
+    //  }
     if (imageUpload == null) return;
     const imageRef = ref(storage, `images/${spot}/${v4()}`);
     uploadBytes(imageRef, imageUpload).then(() => {
       console.log("Image Uploaded");
-      window.location.replace(`/spot/${spot}/`);
+      console.log(imageUpload);
+     window.location.replace(`/spot/${spot}/`);
     });
    
   };
 
+ 
   return (
     <div className="globalTopMargin">
       <div style={{ marginTop: "1rem" }}>
-        <Input
+            <Input
           type="file"
           accept=".png, .jpg, .jpeg"
           onChange={(event) => {
             setImageUpload(event.target.files[0]);
           }}
-        />
-
-        <div style={{ marginTop: "1rem" }}>
-          <div>
-            <Button color="primary" onClick={handleUpload}>
-              Submit
-            </Button>
-          </div>
+            />
         </div>
-      </div>
+       
+        <div style={{ marginTop: "1rem" }}>
+            {check ? (<div>File too large!</div>) : (<p></p>)
+              
+    
+      
+}
+<Button color="primary" onClick={handleUpload}>
+                  Submit
+                </Button></div>
     </div>
   );
 }
