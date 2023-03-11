e46b2721b273d118e6fee6633e9e70207c96f20a
Bee <69877469+bee-dmg@users.noreply.github.com>
2023-03-10 19:59:06 -0500

    adding help

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,7 +1,8 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { onAuthStateChanged, signOut } from "firebase/auth";
 import { auth } from "../firebase-config";
 import Home from "../HomeRouter";
+import { Link } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 import Loading from "../graphics/Loading";
 import LoginForm from "./LoginForm";
@@ -9,6 +10,7 @@ import LoginForm2 from "./LoginForm2";
 import EditProfile from "../User/EditProfile";
 export default function Login(props) {
   const { user, setUser } = useContext(AuthContext);
+  const [help, setHelp]=useState(false);
   const guestQuery = props.queryString;
 console.log(guestQuery)
   onAuthStateChanged(auth, (currentUser) => {
@@ -20,7 +22,7 @@ console.log(guestQuery)
   }
   if (user === null) {
     //returns Login Form to prompt user to sign in, checks if user is null if you use !user to check the logic breaks
-    return (<div><LoginForm /><p className="loginBrowserInfo">Firefox/Safari is recommended.</p><p className="loginFormInfo"> To sign up, enter your desired username and password and hit "sign up". If you have an account, select "Forgot Password" and enter the email you tried to sign in with, it should email you with a non discript email from google about a password reset. Follow the steps and use the password here.</p></div>);
+    return (<div><LoginForm /><p className="loginBrowserInfo">Firefox/Safari is recommended.</p><Link to="#"><span onClick={()=>{setHelp(!help)}}>Help?</span></Link>{help?<div><p className="loginFormInfo"> To sign up, enter your desired username and password and hit "sign up". If you have an account, select "Forgot Password" and enter the email you tried to sign in with, it should email you with a non discript email from google about a password reset. Follow the steps and use the password here.</p></div>:<div></div>}</div>);
   }
   if (user.displayName) {
     //Takes user to Dashboard
